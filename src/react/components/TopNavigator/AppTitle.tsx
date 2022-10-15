fc5da2fad9361a4bfe1de2e34eaf4002257d8db8
Sergey Dolin <sergey.dolin@gmail.com>
2022-10-15 20:04:15 +0200

    Responsive app title

---
+++ src/react/components/TopNavigator/AppTitle.tsx
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 by s4y.solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import {Typography, useMediaQuery} from '@mui/material';
+import T from '../../../l10n';
+import log from '../../../log';
+import {getDi} from '../../../di/default';
+import useObservable from '../../hooks/useObservable';
+import {Route} from '../../../router';
+import {map} from 'rxjs/operators';
+
+const di = getDi();
+const router = di.router;
+
+const appTitle = T`App title`;
+const flexGrow1 = {flexGrow: 1};
+
+const AppTitle: React.FunctionComponent =
+  (): React.ReactElement => {
+    log.render('AppTitle');
+
+
+    const routeTitle = useObservable(
+      router.observableCurrentRoute.pipe(map((route: Route) => route.title)),
+      router.currentRoute.title,
+    );
+
+    const wideScreen = useMediaQuery('(min-width:600px)');
+    return <Typography component="h1" sx={flexGrow1} variant="h6" >
+      {wideScreen ? `${appTitle} - ${routeTitle}` : appTitle}
+    </Typography >;
+  };
+
+export default AppTitle;
--- src/react/components/TopNavigator/index.tsx
+++ src/react/components/TopNavigator/index.tsx
@@ -15,11 +15,8 @@
  */
 
 import React, {useRef} from 'react';
-import {AppBar, Box, IconButton, Toolbar, Typography} from '@mui/material';
-import T from '../../../l10n';
+import {AppBar, Box, IconButton, Toolbar} from '@mui/material';
 import useObservable from '../../hooks/useObservable';
-import {Route} from '../../../router';
-import {map} from 'rxjs/operators';
 import MenuIcon from '@mui/icons-material/Menu';
 import {LessonStatistics} from '../../../tutor';
 import usePromise from '../../hooks/usePromise';
@@ -27,16 +24,15 @@ import Statistics from './Statistics';
 import MenuMain from './MenuMain';
 import {getDi} from '../../../di/default';
 import log from '../../../log';
+import AppTitle from './AppTitle';
 
 const di = getDi();
-const router = di.router;
 const learningDb = di.learningDb;
 const tutor = di.tutor;
 const uiState = di.uiState;
 
-const mr2 = {mr: 2};
 const flexGrow1 = {flexGrow: 1};
-const appTitle = T`App title`;
+const mr2 = {mr: 2};
 
 
 const handleMainMenuClick = () => {
@@ -53,11 +49,6 @@ const TopNavigator: React.FunctionComponent = (): React.ReactElement => {
   const menuOpen = useObservable<boolean>(uiState.observableMainMenuOpen, uiState.mainMenuOpen);
 
   const [currentLessonStatistic] = usePromise<LessonStatistics>(getLessonStatisticsPromise, {total: NO_DATA, wrong: 0});
-
-  const routerTitle = useObservable(
-    router.observableCurrentRoute.pipe(map((route: Route) => route.title)),
-    router.currentRoute.title,
-  );
 
   const mainMenuButtonRef = useRef<HTMLButtonElement>(null);
 
@@ -82,9 +73,7 @@ const TopNavigator: React.FunctionComponent = (): React.ReactElement => {
 
         <MenuMain anchorEl={mainMenuButtonRef.current} />
 
-        <Typography component="h1" sx={flexGrow1} variant="h6" >
-          {`${appTitle} - ${routerTitle}`}
-        </Typography >
+        <AppTitle />
 
         {currentLessonStatistic.total !== NO_DATA && <Statistics initial={currentLessonStatistic} />}
       </Toolbar >
