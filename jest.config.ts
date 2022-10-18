e69dd23a6e32d261aad63a55653d14778e7d139c
Sergey Dolin <sergey.dolin@gmail.com>
2022-10-18 18:40:36 +0200

    Prepare Jest test

---
+++ jest.config.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 by s4y.solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+
+  moduleDirectories: ['node_modules'],
+
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  // The root of your source code, typically /src
+  // `<rootDir>` is a token Jest substitutes
+  roots: ['<rootDir>/src'],
+
+  setupFiles: ['fake-indexeddb/auto'],
+  // Runs special logic, such as cleaning up components
+  // when using React Testing Library and adds special
+  // extended assertions to Jest
+  setupFilesAfterEnv: [],
+
+  testEnvironment: 'jsdom',
+
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+
+  verbose: true,
+};
+
+export default config;
