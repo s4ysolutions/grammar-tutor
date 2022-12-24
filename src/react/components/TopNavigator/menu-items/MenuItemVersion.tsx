f0654f73e79b9bb0dfc93aecebbb268af4faef96
Sergey Dolin <sergey.dolin@gmail.com>
2022-12-24 14:35:15 +0100

    Add студент

--- src/react/components/TopNavigator/menu-items/MenuItemVersion.tsx
+++ src/react/components/TopNavigator/menu-items/MenuItemVersion.tsx
@@ -19,7 +19,7 @@ import {Typography} from '@mui/material';
 
 const MenuItemVersion: React.FunctionComponent =
   (): React.ReactElement => <Typography align="center" variant="body2">
-    v20221214
+    v20221224
   </Typography >;
 
 export default MenuItemVersion;
